d8f8951a52277c9f9c254855560287d761d45384
rezasetyawan <setyawanreza960@gmail.com>
2025-07-22 20:11:20 +0700

    add: music

--- music-list.js
+++ music-list.js
@@ -1,5 +1,7 @@
 // tambah lagu disini contoh "my-new-song.mp3"
 const musicFiles = [
+  "Focus.mp3",
+  "Classic.mp3",
   "Daniel Caesar, Rex Orange County - Rearrange My World (Audio) [ ezmp3.cc ].mp3",
   "Billie Eilish - WILDFLOWER (Official Lyric Video).mp3",
   "Shotgun Willy - Oreo (Don't worry be happy) Lyrics.mp3",
